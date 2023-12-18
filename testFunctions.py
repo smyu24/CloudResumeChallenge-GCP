27c3a20c4c98367f7e79c1047e157aa337a76c0d
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2023-12-18 11:35:25 -0800

    api changes

--- testFunctions.py
+++ testFunctions.py
@@ -6,11 +6,10 @@ class TestVisitorFunctions(unittest.TestCase):
     
     @patch('cloudFunctions.firestore.Client')
     def test_get_visitor_count(self, mock_firestore_client):
-        # Mocking Firestore client and its behavior
+        # Mocking Firestore client / document
         mock_firestore_instance = MagicMock()
         mock_firestore_client.return_value = mock_firestore_instance
         
-        # Mocking the behavior of Firestore document
         mock_document = MagicMock()
         mock_document.exists = True
         mock_document.to_dict.return_value = {'visitorNumber': 5}
@@ -19,25 +18,24 @@ class TestVisitorFunctions(unittest.TestCase):
         mock_collection.document.return_value = mock_document
         mock_firestore_instance.collection.return_value = mock_collection
 
-        # Testing when document exists in Firestore
+        # exisit case
         result = queryCount()
         self.assertEqual(result, 1)
 
-        # Testing when document doesn't exist in Firestore
+        # dne case
         mock_document.exists = False
         result = queryCount()
         self.assertEqual(result, 1)
 
     @patch('cloudFunctions.firestore.Client')
     def test_save_visitor_data(self, mock_firestore_client):
-        # Mocking Firestore client and its behavior
         mock_firestore_instance = MagicMock()
         mock_firestore_client.return_value = mock_firestore_instance
         
         mock_collection = MagicMock()
         mock_firestore_instance.collection.return_value = mock_collection
 
-        # Testing saving visitor data
+        # Update field
         updateCount(10)
         mock_collection.document.assert_called_with('W7oWxRUwYew0tArbY1bd')
         mock_collection.document().set.assert_called_once_with({'visitorNumber': 10})
